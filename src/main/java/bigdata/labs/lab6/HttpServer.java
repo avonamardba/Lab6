1acb2bb34430e155dae27ab5373983e62a3af71e
avonamardba <avonamardba@gmail.com>
2021-12-25 12:21:51 +0300

    Scripted auto-commit on change (2021-12-25 12:21:51) by gitwatch.sh

--- src/main/java/bigdata/labs/lab6/HttpServer.java
+++ src/main/java/bigdata/labs/lab6/HttpServer.java
@@ -1,2 +1,17 @@
-package bigdata.labs.lab6;public class HttpServer {
+package bigdata.labs.lab6;
+
+import java.time.Duration;
+
+public class HttpServer {
+    private static final String PATH = "";
+    private static final String HOST = "localhost";
+    private static final String URL = "url";
+    private static final String COUNT = "count";
+    private static final Duration TIMEOUT = Duration.ofMillis(5000);
+
+    private final ActorRef actorCfg;
+    private final
+    private final
+    private final
+
 }
